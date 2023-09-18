a76f8ddfe9fa8f07493782d8cf1b17cb86c3c287
Kim Taeyoon <xodbs97@gmail.com>
2023-09-18 19:34:46 +0900

    create introduce taeyoon.java

---
+++ src/IntroduceTaeyoon.java
@@ -0,0 +1,10 @@
+
+public class IntroduceTaeyoon {
+
+	public static void main(String[] args) {
+		System.out.println("안녕하세요 저는 김태윤입니다.");
+		System.out.println("저는 26살입니다.");
+		System.out.println("저는 서울에 살고 있습니다.");
+	}
+
+}
