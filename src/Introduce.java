f2f7d9c48ebdd4caaabb31f71faaf4b1e038ddaf
carelesspark <kiae1234@naver.com>
2023-09-18 19:25:47 +0900

    # Self Introduce-박종혁

---
+++ src/Introduce.java
@@ -0,0 +1,12 @@
+
+public class Introduce {
+
+	public static void main(String[] args) {
+		System.out.println("안녕하세요 저는 박종혁입니다.");
+		System.out.println("저는 22살입니다.");
+		System.out.println("저는 안산에 살고 있습니다.");
+		
+
+	}
+
+}
