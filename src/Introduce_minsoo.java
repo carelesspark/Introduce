c918d06b2fad76e7cbf9ef803c80e6e4eeddd4eb
MS5682 <mss5682@naver.com>
2023-09-18 19:39:14 +0900

    create Introduce_minsoo.java

---
+++ src/Introduce_minsoo.java
@@ -0,0 +1,12 @@
+
+public class Introduce {
+
+	public static void main(String[] args) {
+		System.out.println("안녕하세요 저는 송민수입니다.");
+		System.out.println("저는 24살입니다.");
+		System.out.println("저는 서울에 살고 있습니다.");
+		
+
+	}
+
+}
